3d3ff3c082b9466d77f6d2679600737604cfaac9
Naveen-gale <galennave@gmail.com>
2026-06-23 22:49:13 +0530

    test2 don

--- test2.sql
+++ test2.sql
@@ -1,2 +1,33 @@
-SELECT prodect_name, price from flicart where 
+SELECT prodect_name, price
+FROM flicart
+WHERE price = (
+    SELECT MIN(price)
+    FROM flicart
+);
 
+
+select avg(price) from  flicart WHERE prodect_name in ('Laptop','Mobile','Tablet')
+
+
+SELECT * from flicart
+
+
+select prodect_name, stock from flicart where stock >= 10 and available = true and price = 1000
+SELECT prodect_name, price
+FROM flicart
+WHERE categray = 'Electronics'
+AND price = (
+    SELECT MAX(price)
+    FROM flicart
+    WHERE categray = 'Electronics'
+);
+
+SELECT prodect_name
+FROM flicart
+WHERE categray = 'Electronics'
+
+UNION ALL
+
+SELECT prodect_name
+FROM flicart
+WHERE categray = 'Stationery';
